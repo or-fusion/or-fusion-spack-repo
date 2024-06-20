9c18c1b54fcdba6087f8935b5b4a6cddb5c70bb0
whart222 <whart222@gmail.com>
2024-06-20 05:59:10 -0600

    Fixing version url

--- repo/scip/package.py
+++ repo/scip/package.py
@@ -10,17 +10,22 @@ class Scip(CMakePackage):
     """SCIP optimization solver."""
 
     homepage = "https://github.com/scipopt/scip"
-    url = "https://github.com/scipopt/scip/archive/refs/tags/v901.tar.gz"
+    url = "https://github.com/scipopt/scip/archive/v901.tar.gz"
     git = "https://github.com/scipopt/scip.git"
     maintainers("whart222")
+
+    license("Apache-2.0")
+
+    version("9.0.1", sha256="08ad3e7ad6f84f457d95bb70ab21fa7fc648dd43103099359ef8a8f30fcce32e")
 
     depends_on("soplex@7.0.1:", type="build")
     depends_on("metis@5.1.0:", type="build")
     depends_on("ipopt+mumps@3.14.9:", type="build")
     depends_on("gurobi", type="build")
-    license("Apache-2.0")
 
-    version("9.0.1", sha256="X10175ad66e7f113ac5dc00c9d6650a620663a6884fbf2942d6eb7a3d854604f")
+    def url_for_version(self, version):
+        url = "https://github.com/scipopt/scip/archive/v{0}.tar.gz"
+        return url.format(version.joined, version)
 
     def cmake_args(self):
         args = []
